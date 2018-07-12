0fd3faca7f9889ca984005eb95f0a5b0631d2189
Ahmed Sabbour <sabbour@outlook.com>
2018-07-12 11:23:22 +0400

    removed branch name from pull request build tag

--- tasks/updatebuildnumber/index.ts
+++ tasks/updatebuildnumber/index.ts
@@ -5,8 +5,6 @@ async function run() {
     try {
         var buildReason = tl.getVariable("Build.Reason");
         var sourceBranchName = tl.getVariable("Build.SourceBranchName");
-        var targetePullRequestBranchName = getTargetBranchName();
-        //var gitCommitId = tl.getVariable("Build.SourceVersion");
         var buildId = tl.getVariable("Build.BuildId");
         var prNumber = tl.getVariable("System.PullRequest.PullRequestId");
         if(prNumber == undefined)
@@ -17,42 +15,26 @@ async function run() {
         console.log("buildReason: " + buildReason);
         console.log("buildId: " + buildId);
         console.log("sourceBranchName: " + sourceBranchName);
-        console.log("targetePullRequestBranchName: " + targetePullRequestBranchName);
         console.log("prNumber: " + prNumber);
 
 
         var buildTag = "";
 
-        // Shorten the commit id
-        /*
-        var shortCommitId = gitCommitId;
-        if(gitCommitId.length>3)
-            shortCommitId = gitCommitId.substring(0,3);
-        */
         // If it is a pull request
         if(buildReason == "PullRequest" && prNumber != null) {
-            buildTag = targetePullRequestBranchName+"-pr-" + prNumber + "-" + buildId;
+            buildTag = "pr-" + prNumber + "-" + buildId;
         }
         else {
             buildTag = sourceBranchName + "-" + buildId;
         }
 
         // Set it
+        console.log("buildTag: " + buildTag.toLowerCase());
         console.log("##vso[build.updatebuildnumber]"+buildTag.toLowerCase());
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
-}
-function getTargetBranchName() {
-    let targetBranch: string = tl.getVariable('System.PullRequest.TargetBranch');
-    
-    if(targetBranch == undefined)
-        return null;
-    if (!targetBranch.startsWith('refs/heads/'))
-        return null;
-
-    return targetBranch.replace('refs/heads/', '');
 }
 
 run();
