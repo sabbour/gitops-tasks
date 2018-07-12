372e59ca25fd4d7011418af3d62cdf7a999d5721
Ahmed Sabbour <sabbour@outlook.com>
2018-07-12 14:08:12 +0400

    Added includeBuildNumber option

--- tasks/outputenvironmentns/index.ts
+++ tasks/outputenvironmentns/index.ts
@@ -6,16 +6,26 @@ async function run() {
         var serviceName: string = tl.getInput("servicename",true);
         var env: string = tl.getInput("environment");
         var buildNumber = tl.getVariable("Build.BuildNumber");
+        var includeBuildNumber = tl.getBoolInput("includeBuildNumber", true);
 
         console.log("serviceName: " + serviceName);
         console.log("env: " + env);
         console.log("buildNumber: " + buildNumber);
+        console.log("includeBuildNumber: " + includeBuildNumber);
 
         var namespace = "";
-        if(env != undefined && env != null && env.trim() != "")
-            namespace = serviceName + "-" + env + "-" + buildNumber;
-        else
-            namespace = serviceName + "-" + buildNumber;
+        if(includeBuildNumber) {
+            if(env != undefined && env != null && env.trim() != "")
+                namespace = serviceName + "-" + env + "-" + buildNumber;
+            else
+                namespace = serviceName + "-" + buildNumber;
+        }
+        else {
+            if(env != undefined && env != null && env.trim() != "")
+                namespace = serviceName + "-" + env;
+            else
+                namespace = serviceName;
+        }
 
         // Set it
         console.log("Generated namespace: " + namespace.toLowerCase());
