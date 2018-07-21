2f5029d17f379f7512eb46755b59205e4056c04c
Ahmed Sabbour <sabbour@outlook.com>
2018-07-21 13:32:55 -0700

    Updated gitignore

--- tasks/cleanuppreview/index.ts
+++ tasks/cleanuppreview/index.ts
@@ -9,6 +9,12 @@ async function run() {
         var commitId: string = tl.getVariable('Build.SourceVersion');
         console.log("commitId: " + commitId);
 
+        // If no commit id, probably this is a manual build
+        // nothing to do here, moving on
+        if(commitId == undefined) {
+            tl.setResult(tl.TaskResult.Succeeded, "Skipping. No commit id, nothing to do here.");
+        }
+
         prNumber = await gitops.getPullRequestIdByCommitIdAsync(commitId);
         console.log("prNumber: " + prNumber);
     }
